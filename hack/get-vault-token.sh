bae2956d4704c6cff4d3820368f3291a558fc0fe
Simon Weald <glitchcrab-github@simonweald.com>
2024-08-09 14:39:12 +0100

    add check to ensure vault is unsealed before trying to get token

--- hack/get-vault-token.sh
+++ hack/get-vault-token.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+echo -e "\n[INF] checking vault status"
+
+vault status > /dev/null 2>&1
+if [ "$?" -ne 0 ]; then
+   echo "[ERR]: vault is not accessible"
+   exit
+else
+   echo "[INF]: vault is unsealed, continuing"
+fi
+
 TOKEN=$(rbw get "Vault token")
 
 export VAULT_TOKEN=${TOKEN}
